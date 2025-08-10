fdaa175a152799e861de682ecffba66edf803be8
SukunaScriptRoblox <lilabenrathva1989@gmail.com>
2025-08-10 12:34:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,4 +38,6 @@ app.post("/generate", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("🚀 API running on port 3000"));
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`🚀 API running on port ${process.env.PORT || 3000}`);
+});
